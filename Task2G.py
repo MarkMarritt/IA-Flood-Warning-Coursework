fd9a667610a19b6e08b9ee199ef01203ad4eaabb
Khughes127 <123196893+Khughes127@users.noreply.github.com>
2023-02-28 12:50:08 +0000

    Did checks for 2G

--- Task2G.py
+++ Task2G.py
@@ -10,23 +10,36 @@ from matplotlib import dates
 from floodsystem.analysis import datesToTime
 import copy
 
-def run(p,h,n):
+def run(p,h,n,pred=True):
+    if (type(p)==int) and (p>0):
+        pass
+    else:
+        raise ValueError("Invalid value of p entered")
+    if (type(h)==int) and (h>0):
+        pass
+    else:
+        raise ValueError("Invalid value of h entered")
+    if (type(n)==int) and (n>0):
+        pass
+    else:
+        raise ValueError("Invalid value of n entered")
+    
     stations = build_station_list()
     update_water_levels(stations)
     highFlowStations = stations_highest_rel_level(stations, n) #gets stations with current largest relative flow
     # its not practical to find the predicted rel flow of all stations as there are over 2000 and it takes time to find predicted relative flow for each station
     # especially since the predicted change is not going to drastically change the relative flow, taking the past n amount should be enough to find the stations most at risk
 
-        
-    for station in highFlowStations:
-        Dates, Levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days=2)) # fetches the levels for the station from past 10 days
+    if pred==True:   
+        for station in highFlowStations:
+            Dates, Levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days=10)) # fetches the levels for the station from past 10 days
 
-        
-        
-        times,shift=datesToTime(Dates[:4*h])           #times are in units of 15mins so x hours ago is 4x in the list, ill pick 12 hours ago
-        poly, shift = polyfit(Dates[:4*h], Levels[:4*h], p)      # finds polynomial (usually 1 so line of best fit) for data in the past h hours
-        predicted_l2=poly(times[0]+h/24)-poly(times[0])+Levels[0] #adds on change in the past h hours from line of best fit to latest level
-        station.latest_level=predicted_l2   #sets current level to predicted level so stations_level_over_threshold function can be used
+            
+            
+            times,shift=datesToTime(Dates[:4*h])           #times are in units of 15mins so x hours ago is 4x in the list, ill pick 12 hours ago
+            poly, shift = polyfit(Dates[:4*h], Levels[:4*h], p)      # finds polynomial (usually 1 so line of best fit) for data in the past h hours
+            predicted_l2=poly(times[0]+h/24)-poly(times[0])+Levels[0] #adds on change in the past h hours from line of best fit to latest level
+            station.latest_level=predicted_l2   #sets current level to predicted level so stations_level_over_threshold function can be used
     
     pred_lvl_above_0=stations_level_over_threshold(highFlowStations,0)
     for i in pred_lvl_above_0:
